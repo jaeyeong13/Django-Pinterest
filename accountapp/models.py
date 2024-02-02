07f4d8614a72bd47a1ecdfe18c104ce03ac2ed42
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-02-03 02:21:49 +0900

    feat: accountapp\models.py에 새로운 class 추가, DB와 연동

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class HelloWorld(models.Model):
+  text = models.CharField(max_length=255, null=False)   # null=False라는 것은 그 칸이 비어 있으면 안된다는 뜻
